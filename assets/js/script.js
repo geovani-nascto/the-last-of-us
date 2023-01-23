dd7bbce5cc8aec8e49fefaed4ea2aaf7aa7024de
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-01-23 01:18:41 -0300

    js finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 const trailerButton = document.querySelector('.trailer-button');
 const modal = document.querySelector('.modal');
 const closeVideo = document.querySelector('.close-video');
+const video = document.getElementById('video');
+const videoLink = video.src;
+
+/* Uma opção mais curta:
+function modalAlternate(){
+    //toggle = alternar
+    modal.classList.toggle('opened');
+
+}*/
 
 trailerButton.addEventListener('click', () => {
     //Add classe
     modal.classList.add('opened');
+    video.setAttribute('src', videoLink);
 });
 
 closeVideo.addEventListener('click', function(){
+    //modalAlternate();
     modal.classList.remove('opened');
+    video.setAttribute('src', 'none')
 })
